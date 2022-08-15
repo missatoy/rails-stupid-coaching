07b68ba40c5b6c36e01486143721cd1e0920e271
rubensy <86684948+rubensy@users.noreply.github.com>
2022-08-15 14:31:19 +0200

    managed to build stupid coach with 3 different answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'questions/ask'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :asnwer
   # root "articles#index"
 end
